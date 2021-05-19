e12e00ddaab0f748f8d40704498c31e6c42980dc
Denis Shupletsov <shuden99@icloud.com>
2021-05-19 16:41:05 +0300

    1

---
+++ Assets/Scripts/CherecterControler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherecterControler : MonoBehaviour
+{
+    private Rigidbody _playerRigidbody;
+    private Animator _playerAnimator;
+    private const float _originalPlayerSpeed = .1f;
+    private Transform _playerTransform;
+    [SerializeField] private Transform _cameraTransform;
+
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerTransform = GetComponent<Transform>();
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCherecter(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void MoveCherecter(float horizontalInput, float verticalInput) 
+    {
+        float _playerSpeed = _originalPlayerSpeed + (0.05f * Input.GetAxis("Runing"));
+
+        Vector3 _cameraPosition = new Vector3(_cameraTransform.position.x, _playerTransform.position.y, _cameraTransform.position.z);
+
+        Vector3 diractionForward = (_playerTransform.position - _cameraPosition) * verticalInput;
+        Vector3 diractionRight = _cameraTransform.right * horizontalInput;
+        Vector3 diraction = diractionRight + diractionForward;
+
+       _playerRigidbody.MovePosition(_playerTransform.position + diraction.normalized * _playerSpeed);
+
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            _playerTransform.forward = diraction.normalized;
+
+            _playerAnimator.SetBool("IsWalking", true);
+
+            if (Input.GetAxis("Runing") > 0)
+                _playerAnimator.SetBool("IsRuning", true);
+            else
+                _playerAnimator.SetBool("IsRuning", false);
+        }
+        else 
+            _playerAnimator.SetBool("IsWalking", false);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform;
+    private Vector3 _correctPosition;
+    //[SerializeField] private Quaternion _correctRotation;
+    private Transform _cameraTransform;
+
+    private float angle = -1.5f;
+    private float angle1 = 4f;
+    private const float speed = 0.1f;
+    private const float _originalRadius = 4f;
+    private float _radius;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _cameraTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        angle1 = Mathf.Clamp(angle1, -0.5f, 3f) + speed * Input.GetAxis("CameraVertical");
+        angle += -speed * Input.GetAxis("CameraHorizontal");
+        _radius = _originalRadius + Mathf.Sin(angle1);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 B = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+        _cameraTransform.position = _targetTransform.position + (B * _radius) + new Vector3(0, angle1, 0);
+        _cameraTransform.LookAt(_targetTransform);        
+    }
+}
